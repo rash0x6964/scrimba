c3ccf2351f4036a47075abcb03969a314b282146
rash0x6964 <rash0x6964@gmail.com>
2024-01-12 22:08:55 +0100

    add array of post to js file

---
+++ learning-journal/index.js
@@ -0,0 +1,37 @@
+const posts = [
+  {
+    urlImg: "images/article-image-hero.png",
+    date: "july 23,2022",
+    title: "My new journey as a bootcamp student.",
+    paragraph:
+      "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+  },
+  {
+    urlImg: "images/blog-image-01.png",
+    date: "july 23,2022",
+    title: "Blog one",
+    paragraph:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+	{
+    urlImg: "images/blog-image-02.png",
+    date: "july 23,2022",
+    title: "Blog two",
+    paragraph:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+	{
+    urlImg: "images/blog-image-03.png",
+    date: "july 23,2022",
+    title: "Blog three",
+    paragraph:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+	{
+    urlImg: "images/blog-image-01.png",
+    date: "july 23,2022",
+    title: "Blog one",
+    paragraph:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+]
